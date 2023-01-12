148652f36a443bba50f0d04055a0f4502bf0fc15
Dan Jones <59394454+DannyWPWS@users.noreply.github.com>
2023-01-12 17:59:42 +0000

    Resetting branch

--- admin/templates/admin-default.php
+++
@@ -1,5 +0,0 @@
-<div class="wrap ppm-wrap">
-	<div class="page-head">
-		<h2><?php _e( 'WPassword', 'ppm-wp' ); ?></h2>
-	</div>
-</div>
--- admin/templates/views/locked-users.php
+++
@@ -1,7 +0,0 @@
-<div class="wrap ppm-wrap">
-	<div class="page-head">
-		<h2><?php _e( 'WPassword Locked Users', 'ppm-wp' ); ?></h2>
-	</div>
-
-	<?php include_once PPM_WP_PATH . 'admin/templates/views/inactive-users.php'; ?>
-</div>
