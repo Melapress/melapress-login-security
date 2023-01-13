d3c273bc10114979bb6710d0ac3433eb343ce80a
DannyWPWS <DannyWPWS@users.noreply.github.com>
2023-01-13 11:47:46 +0000

    [create-pull-request] automated changes sync

---
+++ app/helpers/class-ppm-mb-string-helper.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @package wordpress
+ * @subpackage wpassword
+ */
+
+defined( 'ABSPATH' ) or die();
+
+if ( ! class_exists( 'PPM_MB_String_Helper' ) ) {
+
+	/**
+	 * Helper object for some string manipulations.
+	 */
+	class PPM_MB_String_Helper {
+
+		public static function mbStrShuffle( $string ) {
+			$chars = self::mbGetChars( $string );
+			shuffle( $chars );
+
+			return implode( '', $chars );
+		}
+
+		private static function mbGetChars( $string ) {
+			$chars = [];
+
+			for ( $i = 0, $length = mb_strlen( $string ); $i < $length; ++ $i ) {
+				$chars[] = mb_substr( $string, $i, 1, 'UTF-8' );
+			}
+
+			return $chars;
+		}
+
+		/**
+		 * Helper function which basically replaces mb_str_split but is compatible with PHP older than7.4.
+		 */
+	  public static function mbSplitString( $string, $split_length = 1, $encoding = null ) {
+		    if (null !== $string && !\is_scalar($string) && !(\is_object($string) && \method_exists($string, '__toString'))) {
+		        trigger_error('mb_str_split(): expects parameter 1 to be string, '.\gettype($string).' given', E_USER_WARNING);
+		        return null;
+		    }
+		    if (null !== $split_length && !\is_bool($split_length) && !\is_numeric($split_length)) {
+		        trigger_error('mb_str_split(): expects parameter 2 to be int, '.\gettype($split_length).' given', E_USER_WARNING);
+		        return null;
+		    }
+		    $split_length = (int) $split_length;
+		    if (1 > $split_length) {
+		        trigger_error('mb_str_split(): The length of each segment must be greater than zero', E_USER_WARNING);
+		        return false;
+		    }
+		    if (null === $encoding) {
+		        $encoding = mb_internal_encoding();
+		    } else {
+		        $encoding = (string) $encoding;
+		    }
+
+		    if (! in_array($encoding, mb_list_encodings(), true)) {
+		        static $aliases;
+		        if ($aliases === null) {
+		            $aliases = [];
+		            foreach (mb_list_encodings() as $encoding) {
+		                $encoding_aliases = mb_encoding_aliases($encoding);
+		                if ($encoding_aliases) {
+		                    foreach ($encoding_aliases as $alias) {
+		                        $aliases[] = $alias;
+		                    }
+		                }
+		            }
+		        }
+		        if (! in_array($encoding, $aliases, true)) {
+		            trigger_error('mb_str_split(): Unknown encoding "'.$encoding.'"', E_USER_WARNING);
+		            return null;
+		        }
+		    }
+
+		    $result = [];
+		    $length = mb_strlen($string, $encoding);
+		    for ($i = 0; $i < $length; $i += $split_length) {
+		        $result[] = mb_substr($string, $i, $split_length, $encoding);
+		    }
+		    return $result;
+		}
+	}
+}
