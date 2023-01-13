bb6444e124b714d5f4876ee525ae4f87e5540c72
DannyWPWS <DannyWPWS@users.noreply.github.com>
2023-01-13 15:21:01 +0000

    [create-pull-request] automated changes sync

---
+++ admin/templates/help/contact-us.php
@@ -0,0 +1,25 @@
+<?php
+	// Plugin adverts sidebar
+	require_once 'sidebar.php';
+?>
+<div class="ppm-help-main">
+	<!-- getting started -->
+	<div class="title">
+		<h2><?php _e( 'Contact Us', 'ppm-wp' ); ?></h2>
+	</div>
+	<style type="text/css">
+		.fs-secure-notice {
+			position: relative !important;
+			top: 0 !important;
+			left: 0 !important;
+		}
+		.fs-full-size-wrapper {
+			margin: 10px 20px 0 2px !important;
+		}
+	</style>
+	<?php
+	$freemius_id = ppm_freemius()->get_id();
+	$vars = array( 'id' => $freemius_id );
+	echo fs_get_template( 'contact.php', $vars );
+	?>
+</div>
