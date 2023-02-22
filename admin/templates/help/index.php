b4e540465d59ddb2b971661a6450a28196093ab8
DannyWPWS <DannyWPWS@users.noreply.github.com>
2023-02-22 14:36:03 +0000

    [create-pull-request] automated changes sync

--- admin/templates/help/index.php
+++ admin/templates/help/index.php
@@ -20,9 +20,6 @@
 		<a href="<?php echo esc_url( remove_query_arg( 'tab' ) ); ?>" class="nav-tab<?php echo 'help' === $current_tab ? ' nav-tab-active' : ''; ?>"><?php esc_html_e( 'Help', 'ppm-wp' ); ?></a>
 		<?php
 		?>
-		<a href="<?php echo esc_url( add_query_arg( 'tab', 'contact-us' ) ); ?>" class="nav-tab<?php echo 'contact-us' === $current_tab ? ' nav-tab-active' : ''; ?>"><?php esc_html_e( 'Contact Us', 'ppm-wp' ); ?></a>
-		<?php
-		?>
 		<a href="<?php echo esc_url( add_query_arg( 'tab', 'system-info' ) ); ?>" class="nav-tab<?php echo 'system-info' === $current_tab ? ' nav-tab-active' : ''; ?>"><?php esc_html_e( 'System Info', 'ppm-wp' ); ?></a>
 	</div>
 	<div class="ppm-help-section nav-tabs">
